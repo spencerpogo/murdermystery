ecb62a20e391c69e92b70036e7f701bb512708ff
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-09-16 16:46:23 -0700

    Make entire body night

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,22 +5,24 @@ import styles from "../styles/Home.module.css";
 
 function Home({ t }: { t: (id: string) => string }) {
   return (
-    <div>
+    <>
       <Head>
         <title>Murder Mystery</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.night}>
-        <div className={styles.mainMenu}>
-          <h1 className={styles.title}>{t("title")}</h1>
-          <div>
-            <button className={styles.menuBtn}>{t("join")}</button>
-            <button className={styles.menuBtn}>{t("create")}</button>
+      <body className={styles.night}>
+        <main>
+          <div className={styles.mainMenu}>
+            <h1 className={styles.title}>{t("title")}</h1>
+            <div>
+              <button className={styles.menuBtn}>{t("join")}</button>
+              <button className={styles.menuBtn}>{t("create")}</button>
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </body>
+    </>
   );
 }
 
