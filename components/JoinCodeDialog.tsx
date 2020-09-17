3866ce0775786c98f5fd728fa264c4cc8de2158d
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-09-16 17:05:25 -0700

    Add JoinCodeDialog component

---
+++ components/JoinCodeDialog.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+export default function JoinCodeDialog() {
+  return (
+    <>
+      <input type="text"></input>
+      <button className={styles.menuBtn}></button>
+    </>
+  );
+}
