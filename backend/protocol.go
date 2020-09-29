9337de80186c697d81bbb3ee9af42752736b7105
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-09-28 18:53:36 -0700

    Inital opcode model

--- backend/protocol.go
+++ backend/protocol.go
@@ -5,22 +5,49 @@ import (
 	"fmt"
 )
 
+const (
+	// Hello is a hello
+	hello = 0
+)
+
+// HelloMsg is a hello message
+type HelloMsg struct {
+	name string
+}
+
+func helloHandler(client *Client, data []byte) error {
+	var msg HelloMsg
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return err
+	}
+
+	resp, _ := json.Marshal(map[string]string{"msg": fmt.Sprintf("Hello, %s!", msg.name)})
+	client.send <- resp
+	return nil
+}
+
+func callHandler(client *Client, op int, data []byte) error {
+	switch op {
+	case hello:
+		return helloHandler(client, data)
+	default:
+		return fmt.Errorf("Unrecognized op")
+	}
+}
+
 func decodeMsg(client *Client, msg []byte) error {
 	if len(msg) < 2 {
 		return fmt.Errorf("Message too short")
 	}
 
 	// First byte is opcode
-	//opcode := int(msg[0]) - 65
-	// TODO: validate opcode and don't parse it as JSON
-
-	// Unmarshal as JSON
-	var data map[string]interface{} // TODO: maybe other types?
-	if err := json.Unmarshal(msg, &data); err != nil {
-		return fmt.Errorf("Message Decode Error: %s", err)
+	opcode := int(msg[0]) - 65
+	err := callHandler(client, opcode, msg[1:])
+	// TODO: check error
+	if err != nil {
+		client.Close()
+		return nil
 	}
-
-	// TODO: do something with the JSON
 
 	okJSON, _ := json.Marshal(map[string]bool{"ok": true})
 	client.send <- okJSON
