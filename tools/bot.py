02ec34985a13d03c9cbead90e953058d5e771c61
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-10-14 10:04:05 -0700

    :sparkles: Add start game feature to bot

--- tools/bot.py
+++ tools/bot.py
@@ -12,7 +12,7 @@ def get_name(offset: int):
     return f"Bot{offset}"
 
 
-RPCS = {"setName": "A"}
+RPCS = {"setName": "A", "startGame": "B"}
 
 
 def serialize_rpc(method: str, arg):
@@ -27,11 +27,24 @@ async def send_rpc(ws, method: str, arg):
 async def run_bot(sess: aiohttp.ClientSession, gid: str, offset: int):
     name = get_name(offset)
     print(f"Connecting with name: {name}")
+    is_host = False
     async with sess.ws_connect(server + "/game/" + gid) as ws:
         await send_rpc(ws, "setName", name)
 
         async for msg in ws:
             print(msg.data)
+            data = {}
+            try:
+                data = json.loads(msg.data)
+            except:
+                pass
+            t = data.get("type")
+            if t == "host":
+                is_host = data.get("isHost", False)
+            elif t == "players" and is_host:
+                # Everytime someone joins/leaves, try to start the game
+                #  Doesn't matter if it causes and error
+                await send_rpc(ws, "startGame", {})
 
 
 async def main():
