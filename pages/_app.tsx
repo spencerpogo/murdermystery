425f95535a84c85001274e4342c5c6066ed599cc
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-10-20 16:34:42 -0700

    :label: Use typescript in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import "../styles/globals.css";
 
 import { CSSReset, DarkMode, ThemeProvider } from "@chakra-ui/core";
 
-const MyApp = ({ Component, pageProps }) => {
+import { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider>
       <CSSReset />
