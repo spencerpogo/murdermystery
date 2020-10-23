e9bb6ce0069834304799866bbc4377f1ef261290
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-10-23 14:58:12 -0700

    :bug: Check for protoc only when building golang

--- tools/protoc.sh
+++ tools/protoc.sh
@@ -4,25 +4,26 @@ set -e
 
 # Generates protocol buffers code - YMMV
 # Should work on windows with some modifications
-if [ ! $(command -v protoc) ]; then
-  >&2 echo "You must install protoc into your path before running this script."
-  >&2 echo "You can install it here: https://developers.google.com/protocol-buffers/docs/downloads"
-  exit 1
-fi
-
-if [ ! $(command -v protoc-gen-go) ]; then
-  >&2 echo "You must install protoc-gen-go into your path before running this script."
-  >&2 echo "You can install by running: go get google.golang.org/protobuf/cmd/protoc-gen-go"
-  exit 1
-fi
-
 # TODO: Require pbjs
+
 ROOT=${1:-$(pwd)}
 
 GO_PB_PKG=$ROOT/backend/protocol/pb
 JS_PB_PKG=$ROOT/pbjs
 
 if [[ "$2" != "--no-golang" ]]; then
+  if [ ! $(command -v protoc) ]; then
+    >&2 echo "You must install protoc into your path before running this script."
+    >&2 echo "You can install it here: https://developers.google.com/protocol-buffers/docs/downloads"
+    exit 1
+  fi
+
+  if [ ! $(command -v protoc-gen-go) ]; then
+    >&2 echo "You must install protoc-gen-go into your path before running this script."
+    >&2 echo "You can install by running: go get google.golang.org/protobuf/cmd/protoc-gen-go"
+    exit 1
+  fi
+
   echo "Generating go code..."
   mkdir -p $GO_PB_PKG
   protoc -I=$ROOT --go_out=$GO_PB_PKG $ROOT/*.proto
