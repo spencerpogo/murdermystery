5e0a6cc7e473d7d27170cfe0097e54bfb6ff5859
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-10-30 09:52:05 -0700

    :sparkles: Allow bot to start game

--- tools/bot.js
+++ tools/bot.js
@@ -12,6 +12,7 @@ const getName = (i) => `Bot${i + 1}`;
 const runBot = (gid, i) => {
   const name = getName(i);
   const ws = new NodeWebSocket(`${SERVER}/game/${gid}`);
+  let isHost = false;
 
   const send = (pbMsg) => {
     console.log(`[${name}] ↑`, pbMsg);
@@ -20,7 +21,19 @@ const runBot = (gid, i) => {
 
   ws.on("message", (buffer) => {
     const msg = ServerMessage.decode(buffer);
-    console.log(`[${name}] ↓`, msg);
+    // msg.players gets collapsed so log it top level
+    console.log(`[${name}] ↓`, msg.players ? msg.players : msg);
+
+    if (msg.host && msg.host.isHost) {
+      isHost = true;
+    } else if (
+      msg.players &&
+      isHost &&
+      msg.players.players &&
+      msg.players.players.length >= 6
+    ) {
+      send({ startGame: {} });
+    }
   });
 
   ws.on("open", () => send({ setName: { name } }));
