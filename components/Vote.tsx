6aa340a9e88ccb526f4c2c1420fc40e49f58921f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-10-30 18:04:42 -0700

    :sparkles: Add vote component

--- components/Vote.tsx
+++ components/Vote.tsx
@@ -1,3 +1,60 @@
-export default function Vote({ names }: { names: string[] }) {
-  return <p>Vote: TODO</p>;
+import { Button, Heading, Stack, Text } from "@chakra-ui/core";
+
+import { forcedTranslate as t } from "../translate";
+
+function VoteButton({ text }: { text: string }) {
+  return (
+    <Button mt="2" variantColor="gray">
+      {text}
+    </Button>
+  );
+}
+
+function VotesDisplay({ name, voters }: { name: string; voters: string[] }) {
+  // TODO: Maybe make this a little prettier?
+  return (
+    <>
+      <Heading size="md">{name}</Heading>
+      {voters.map((voter) => (
+        <Text key={voter}>{voter}</Text>
+      ))}
+    </>
+  );
+}
+
+export default function Vote({
+  msg,
+  desc,
+  names,
+  votes,
+}: {
+  msg: string;
+  desc?: string;
+  names: string[];
+  votes: { [name: string]: string[] };
+}) {
+  return (
+    <>
+      <Heading>{t(msg)}</Heading>
+      {desc && <Text mt="2">{t(desc)}</Text>}
+      <Stack mt="2">
+        {names.map((n) => (
+          <VoteButton key={n} text={n} />
+        ))}
+      </Stack>
+      <Heading as="h4" size="lg" mt="3" mb="2">
+        {t("Votes")}
+      </Heading>
+      {Object.keys(votes)
+        .filter((name) => votes[name].length)
+        .map((name) => (
+          <VotesDisplay
+            // I don't know what else to use
+            key={name + "\x00" + votes[name].join("\x00")}
+            name={name}
+            voters={votes[name]}
+          />
+        ))}
+    </>
+  );
 }
