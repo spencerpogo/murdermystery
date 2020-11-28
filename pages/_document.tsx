3721de73cb20dd5c90576b11a94246cbefd63a65
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-28 14:58:31 -0800

    :art: Remove unneeded boilerplate from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
-  static getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    return NextDocument.getInitialProps(ctx);
-  }
-
   render(): JSX.Element {
     return (
       <Html>
