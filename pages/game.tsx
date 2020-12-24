7b46d66de855c11d0f360a200f64b6e1047e868f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-12-24 12:47:42 -0800

    :sparkles: Better host detection for prod environments

--- pages/game.tsx
+++ pages/game.tsx
@@ -9,10 +9,6 @@ import { STRINGS, useTranslator } from "../lib/translate";
 function useGameContent() {
   const t = useTranslator();
   const { query } = useRouter();
-  const { id } = query;
-  const server = query.srv || "ws://localhost:8080";
-  const queryName = (query.name || "").toString();
-
   const [name, setName] = useState<string>("");
 
   const nameComponent = (
@@ -24,6 +20,15 @@ function useGameContent() {
     //  pre-rendered so pre-render the name component which will be shown the most often
     return nameComponent;
   }
+
+  const { id } = query;
+  const host = window?.location?.host || "localhost:3000";
+  const server =
+    query.srv ||
+    (window?.location?.protocol === "https:" ? "wss://" : "ws://") +
+      (host === "localhost:3000" ? "localhost:8080" : host);
+  const queryName = (query.name || "").toString();
+
   if (!name) {
     if (queryName) {
       setName(queryName);
