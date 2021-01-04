87561ec06538e467ad3e3fa98cfa8c2c7e4f4452
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-03 16:13:10 -0800

    :rotating_light: lint-staged: Separate main.proto
    Don't run prettier/eslint on main.proto.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  "{*.{js,jsx,ts,tsx},main.proto}": [
+  "*.{js,jsx,ts,tsx}": [
     "prettier --write",
     "npx eslint --cache --fix",
     () => "npx tsc --noemit",
   ],
+  "main.proto": [() => "npm run go-vet", () => "npx tsc --noemit"],
   "*.{md,yml,json}": "prettier --write",
-  "{backend/**/*.go,main.proto}": () => "npm run go-vet",
+  "backend/**/*.go": () => "npm run go-vet",
 };
